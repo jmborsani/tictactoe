0f082c32f6b4a1bd5f129b84ea2d8f03342f7457
joao.borsani <joao.borsani@corp.globo.com>
2016-09-14 19:25:42 -0300

    Reestruturação de código. E separação de algumas rotinas em métodos.

--- game.py
+++ game.py
@@ -3,105 +3,107 @@
 
 import random
 
-board = [["   ", "   ", "   "],["   ", "   ", "   "],["   ", "   ", "   "]]
+def create_board():
+    return list([" " for i in range(1,10)])
 
-p1 = raw_input("Jogador 1: ")
-p2 = raw_input("Jogador 2: ")
+def get_players():
+    p1 = raw_input("Jogador 1: ")
+    p2 = raw_input("Jogador 2: ")
 
-players = [p1, p2]
-
-print
-print "Jogador {0} é X".format(p1)
-print "Jogador {0} é O".format(p2)
-print
-
-def move(row, column, symb):
-    board[row][column] = symb
+    return [p1, p2]
 
 def print_board(board):
-    print "         |         |         "
-    print "   {0}   |   {1}   |   {2}   ".format(board[0][0], board[0][1], board[0][2])
-    print "---------|---------|---------"
-    print "   {0}   |   {1}   |   {2}   ".format(board[1][0], board[1][1], board[1][2])
-    print "---------|---------|---------"
-    print "   {0}   |   {1}   |   {2}   ".format(board[2][0], board[2][1], board[2][2])
-    print "         |         |         "
+    print "   {0}   |   {1}   |   {2}   ".format(board[0], board[1], board[2])
+    print "{0}|{0}|{0}".format("-" * 7)
+    print "   {0}   |   {1}   |   {2}   ".format(board[3], board[4], board[5])
+    print "{0}|{0}|{0}".format("-" * 7)
+    print "   {0}   |   {1}   |   {2}   ".format(board[6], board[7], board[8])
 
 def how_to_play():
-    board = [["0,0", "0,1", "0,2"],["1,0", "1,1", "1,2"],["2,0", "2,1", "2,2"]]
+    board = list(range(1,10))
     print_board(board)
 
-def get_coordenate(coordenate, board):
-    coordenate = coordenate.split(",")
+def verify_input(user_input, board):
+    position = 0
+
     try:
-        row        = int(coordenate[0])
-        column     = int(coordenate[1])
+        position = int(user_input)
 
-        if board[row][column] != "   ":
-            print "Ops! O amiguinho já jogou aí!"
-            return
-
-        if (row < 0 or row > 2) or (column < 0 or column > 2):
-            print "Ops! Esse campo não existe!"
-            return
+        if (position < 1 or position > 9):
+            position = 0
+            raise IndexError("Ops! Essa posição não existe!")
 
     except ValueError:
-        print "Informe apenas números!"
-        return
+        print "Ops! Informe apenas números!"
 
-    except IndexError:
-        print "Informe as coordenadas corretamente!"
-        return
+    except IndexError as msg:
+        print msg
 
-    return [row, column]
+    return position
 
 def who_wins(board):
     return False
 
-print "Let's play!"
-print
-print_board(board)
+def move(position, symb):
+    pos = position - 1
 
-start_with = random.randint(0,1)
+    if board[pos] != " ":
+        print "Ops! O amiguinho já jogou aí!"
+        return False
 
-if start_with == 1:
-    players[0], players[1] = players[1], players[0]
+    board[pos] = symb
+    return True
 
-i = 0
-while True:
-    i += 1
-    print
-    print "Turno #{0}. (h para help / s para mostrar o tabuleiro)".format(i)
+if __name__ == "__main__":
+    # criando tabuleiro
+    board = create_board()
 
-    for i, player in enumerate(players):
-        while True:
-            coordenate = raw_input("{0}: ".format(player))
+    # pegando os jogadores
+    players = get_players()
 
-            if coordenate == "h":
-                how_to_play()
-                continue
+    # imprimindo o tabuleiro e como jogar
+    print "Let's play!"
+    how_to_play()
 
-            if coordenate == "s":
-                print_board(board)
-                continue
+    # randomizando a ordem de jogada
+    start_with = random.randint(0,1)
 
-            # ToDo: sempre faz essa avaliacao!
-            # Tirar do Loop
-            if i == 0:
-                symb   = " X "
-            else:
-                symb   = " O "
+    if start_with == 1:
+        players[0], players[1] = players[1], players[0]
 
-            coordenate = get_coordenate(coordenate, board)
+    # definicao dos simbolos
+    symbols = ["X", "O"]
 
-            if coordenate == None:
-                how_to_play()
-                continue
+    # imprimido ordem para o jogadores
+    print "Jogador {0} é {1}".format(players[0], symbols[0])
+    print "Jogador {0} é {1}".format(players[1], symbols[1])
 
-            print
-            move(coordenate[0], coordenate[1], symb)
-            print_board(board)
-            break
+    i = 0
+    while True:
+        i += 1
+        print "Turno #{0}. ('h' para Help / 's' para imprimir tabuleiro)".format(i)
 
-    if who_wins(board):
-        continue
+        for j, player in enumerate(players):
+            while True:
+                user_input = raw_input("{0}: ".format(player)).rstrip('\n')
+
+                if user_input == "h":
+                    how_to_play()
+                    continue
+
+                if user_input == "s":
+                    print_board(board)
+                    continue
+
+                position = verify_input(user_input, board)
+
+                if position == 0:
+                    how_to_play()
+                    continue
+
+                if move(position, symbols[j]):
+                    print_board(board)
+                    break
+
+        if who_wins(board):
+            continue
